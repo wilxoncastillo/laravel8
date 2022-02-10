5ade40771012e557b5e6a6493a1d458696835c0d
Wilson Castillo <wilxoncastillo@gmail.com>
2022-02-10 10:26:41 -0500

    route resoruce

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 
-Route::get('/cursos', [CursoController::class, 'index'])->name('cursos.index');
+/* Route::get('/cursos', [CursoController::class, 'index'])->name('cursos.index');
 Route::get('/cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 Route::post('/cursos', [CursoController::class, 'store'])->name('cursos.store');
 Route::get('/cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
 Route::get('/cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
 Route::put('/cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
-Route::delete('/cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+Route::delete('/cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy'); */
+
+Route::resource('cursos', CursoController::class);
